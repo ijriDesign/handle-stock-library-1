f26624d11c155330fe5a015c62aa897c8c2ace64
ndjerrou <n.djerrou@gmail.com>
2023-12-15 18:51:49 +0100

    Exos completed

---
+++ index.js
@@ -0,0 +1,85 @@
+// You are a book seller and you want to create a little program to manage your stock
+
+// You need to create multiple functions to get an overview of your stock
+
+// Our entire library
+const books = require('./library.json');
+
+// @TODO : complete this function
+
+/**
+ * Returns the number of books in stock
+ * @param{array} the books in stock
+ * @returns{boolean} number of books
+ */
+function nbBooks() {}
+
+/**
+ * List efficiently in the console all of the title of the stock
+ * @param{array} the books in stock
+ */
+function listAllTitles() {}
+
+// @TODO : write the following function
+
+/**
+ * Add a book inside the stock
+ * @param{array} the books in stock
+ * @param{string} title of the book
+ * @param{string} author of the book
+ * @param{string} genre of the book
+ * @param{number} pages of the book
+ * @param{string} language of the book
+ * @param{year} year of the published book
+ * @returns{array} the new stock
+ */
+
+// @BONUS : modify now the current stock with the new stock returned by the function written above
+
+// @TODO : write the same function as above but now it only received an object containg all of the information about a new book to add in the stock
+
+// @TODO : write the following function
+
+/**
+ * Add a book inside the stock - new version
+ * @param{object} all of the infos about the new book
+ * @param{array} the new stock
+ */
+
+// @TODO : write the following function
+
+/**
+ * Remove a particular book
+ * @param{array} stock of books
+ * @param{number} index of the book to delete
+ */
+
+// @TODO : write the following function
+
+/**
+ * Update the title of a particular book
+ * @param{array} stock of books
+ * @param{string} new title of the book
+ */
+
+// @TODO : write the following functions
+
+/**
+ * Add a book inside the stock - new version
+ * @param{object} all of the infos about the new book
+ * @param{array} the new stock
+ */
+
+// @TODO : write a function that list in the console only the books written in English
+
+// @TODO : write a function that list in the console  only the books that have more than 300 pages
+
+// @TODO : write a function that list in the console only the books that have more than 300 pages
+
+// @TODO : write a function that returns all of the books written between 2000 (excluded) and 2010
+
+// @TODO write a function that returns all of the books with the genre "Fantasy" and in English
+
+// BONUS:
+
+// @TODO write a function that returns a book that contains the word ride inside it's title
